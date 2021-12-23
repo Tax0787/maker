9858192053c80e4446501853ae7415a16c9d9669
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2021-12-23 10:21:20 +0000

    Correcting errors

--- Maker.py
+++ Maker.py
@@ -1,19 +1,32 @@
+def sum_plus(listes):
+  if type(listes[0]) == str: r = ''
+  elif type(listes[0]) == int: r = 0
+  else: raise TypeError('sum list\'s object is must int or str')
+  for i in listes: r += i
+  return r
+
 class Definer:
-    def __init__(self, name, inputs, sose):
-        self.name = name
-        self.inputs = inputs
-        self.sose = sose
-    lambdas = lambda self: '{} = lambda {}: {}'.format(self.name, self.inputs, self.sose)
+  def __init__(self, name, inputs, sose):
+    self.name = name
+    self.inputs = inputs
+    self.sose = sose
+  lambdas = lambda self: f'{self.name} = lambda {self.inputs}: {self.sose}'
 class MakerMom(Definer):
-    ob1 = Definer('define', 'self' , '\'def {}({}):\\n{}\'.format(self.name, self.inputs, self.sose)')
-    ob2 = Definer('son_class', 'self' , '\'class {}({}):\\n{}\'.format(self.name, self.inputs, self.sose)')
-    exec('{}\n{}'.format(ob1.lambdas(),ob2.lambdas()))
+  ob1 = Definer('define', 'self' , 'f\'def {self.name}({self.inputs}):\\n{self.sose}\'')
+  ob2 = Definer('son_class', 'self' , 'f\'class {self.name}({self.inputs}):\\n{self.sose}\'')
+  ob1 = ob1.lambdas()
+  ob2 = ob2.lambdas()
+  exec(f'{ob1}\n{ob2}')
 class Makers(MakerMom):
-    ob1 = MakerMom('classes', 'self', '    ob = MakerMom(self.name, \'\', self.sose)\n    return ob.son_class()' )
-    ob2 = MakerMom('Error', 'self', '    ob = MakerMom(self.name, \'Exception\', self.sose)\n    return ob.son_class()' )
-    exec('{}\n{}'.format(ob1.define(),ob2.define()))
+  ob1 = MakerMom('classes', 'self', '  ob = MakerMom(self.name, \'\', self.sose)\n  return ob.son_class()' )
+  ob2 = MakerMom('Error', 'self', '  ob = MakerMom(self.name, \'Exception\', self.sose)\n  return ob.son_class()' )
+  ob1 = ob1.define()
+  ob2 = ob2.define()
+  exec(f'{ob1}\n{ob2}')
 
 
 def Maker(name, inputs, sose, option):
-    ob = Makers(name, inputs, sose)
-    return eval('ob.{}()'.format(option))
+  ob = Makers(name, inputs, sose)
+  return eval(f'ob.{option}()')
+
+Makes = lambda make_list: [Maker(i['name'], i['inputs'], i['sose'], i['option']) + '\n' for i in make_list]
